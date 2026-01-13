0375b205c44c86398a8415c2a8903ad5e0fa0b92
kimguen999 <kimguen999@gmail.com>
2026-01-13 17:24:59 +0900

    새íígogo

--- src/Aaa_git.java
+++ src/Aaa_git.java
@@ -1,5 +1,6 @@
 public class Aaa_git {
   public static void main(String[] args) {
     System.out.println("30분 뒤 집간다~~~");
+    System.out.println("20분 뒤 집간다~~~");
   }
 }
--- src/Bbb_git.java
+++ src/Bbb_git.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class Bbb_git {
+  public static void main(String[] args) {
+    System.out.println("가자가자~~");
+  }
 }
